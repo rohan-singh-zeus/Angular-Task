1d157194dea08a643d68867aeabf4aaa7e051f77
Rohan Singh <rohan.singh@zeuslearning.mumbai>
2024-09-27 18:31:06 +0530

    initial commit...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
